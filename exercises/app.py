4a3be58e0b6f5299dde9a67ad654690539bbac72
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:29:03 +0300

    shhjs

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+	My_favorite_players=["Lebron James", "Ryan", "Phelps"] 
+	return render_template("index.html", My_favorite_players=My_favorite_players,likes_same_sport = True)
+    
 
 if __name__ == '__main__':
    app.run(debug = True)
